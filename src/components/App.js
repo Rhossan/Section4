d00b36cc448a7a7fa4c9ce9ac9eadec760eac93d
Rhossan <rhossan93@gmail.com>
2019-02-03 15:39:09 -0500

    added functionality and styling for both views and for  mobile resolution

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import store from '../store/store';
 import data from '../data.json';
 import Search from './Search';
 import Views from './Views';
+import Carousel from './Carousal';
 
 
 class App extends Component {
@@ -18,6 +19,23 @@ class App extends Component {
       searchParams: ''
     }
     this.updateSearchParams = this.updateSearchParams.bind(this);
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(button){
+    const {sliderView, listView} = this.state;
+    switch (button) {
+      case 'slider':
+        if(sliderView) break;
+        this.setState({sliderView: true, listView: false});
+        break;
+      case 'list':
+        if(listView) break;
+        this.setState({listView: true, sliderView: false});
+        break;
+      default:
+        break;
+    }
   }
 
   updateSearchParams(event){
@@ -30,10 +48,9 @@ class App extends Component {
     //   .then(data => this.setState({ data }));
     // console.log('data',data);
     this.setState({ data })
-
   }
   render() {
-    const {searchParams} = this.state;
+    const {searchParams, sliderView, listView} = this.state;
     return (
       <div>
         <header>
@@ -43,13 +60,13 @@ class App extends Component {
         <Provider store={store}>
           <div>
             <h3>Toggle Display</h3>
-            <button>Slider</button>
-            <button>List</button>
+            <button onClick={(e) => this.toggle('slider')}>Slider</button>
+            <button onClick={(e) => this.toggle('list')}>List</button>
           </div>
-          <div>
-            <h1>Headline</h1>
-            <Views {...data} />
-          </div>
+          {/* <div> */}
+            {/* <h1>Headline</h1> */}
+            <Views {...data} sliderView={sliderView} listView={listView} />
+          {/* </div> */}
         </Provider>
       </div>
     );
--- src/components/Views.js
+++ src/components/Views.js
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
 // import { connect } from "react-redux";
+// import Carousal from './Carousal';
+import '../assets/section4fonts.css';
+import Flickity from "react-flickity-component";
+import "../assets/flickity.css";
 
+// const flickityOptions = {
+//   pageDots: false,
+//   wrapAround: true,
+//   cellAlign: 'left',
+  
+// };
 export default class Views extends Component {
         // img  
         // show name 
         // video name
+
+    
     render(){
         const { data } = this.props;
         const carousalMappings = data.map(d => {
             return(
-            <div key={d.timestamp}>
+            <div className='card' key={d.timestamp}>
                 <img src={`/images/${d.backgroundImage}`} alt={d.description}/> 
-                <h2>{d.show}</h2>
-                <h3>{d.name}</h3>
+                <div>
+                    <h2 className='tk-benton-sans-wide'>{d.show}</h2>
+                    <p className='tk-benton-sans-wide'>{d.name}</p>
+                </div> 
             </div>
             );
-        })
+        });
+        const flickityOptions = carousalMappings.length > 4 ? 
+            {
+                pageDots: false,
+                wrapAround: true,
+                cellAlign: 'left'
+            } : 
+            {
+                pageDots: false,
+                wrapAround: false,
+                cellAlign: 'left',
+                prevNextButtons: false
+            };
         return(
-            <React.Fragment>
+            this.props.sliderView ? 
+            <div className='slider-view'>
+                <h1 className='tk-benton-sans-wide'>Headline</h1>
+                <Flickity  options={flickityOptions}>
+                    {carousalMappings}
+                </Flickity>
+            </div> :
+
+             <div className='list-view'>
+                <h1 className='tk-benton-sans-wide'>Headline</h1>
                 {carousalMappings}
-            </React.Fragment>
+             </div>
         );
     }
 }
